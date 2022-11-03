fc0d92b783b543c3fbad7c5a0a5cd6239b470745
DESKTOP-TGSUN6V\user <public@ninetree.com>
2022-11-03 16:15:35 +0900

    datasource 예제 1

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,12 +19,21 @@ class MemberRepositoryVOTest {
     void crud() throws SQLException {
 
         //save
-        Member member = new Member("memberV2", 10000);
+        Member member = new Member("memberV100", 10000);
         repositoryVO.save(member);
 
         //findById
         Member findMember = repositoryVO.findById(member.getMemberId());
         assertThat(findMember).isEqualTo(member);
+
+        //update
+        repositoryVO.update(member.getMemberId(), 20000);
+        Member updateMember = repositoryVO.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repositoryVO.delete(member.getMemberId());
+        assertThatThrownBy(() -> repositoryVO.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 
 }
